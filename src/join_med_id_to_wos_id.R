9ebc107a53930836cef5f20562fb1081f3805239
Matthew Alexander Hutchinson <maahutch@in.cns.iu.edu>
2019-06-05 13:22:50 -0400

    initial commit for matchin medline to wos citations

--- src/join_med_id_to_wos_id.R
+++
@@ -1,8 +0,0 @@
-med_id_to_wos_id <- "SELECT id, article_year, wosid
-                     FROM medline_article_date
-                     JOIN wosid_to_pmid 
-                     ON
-                     medline_article_date.id::varchar = wosid_to_pmid.wosid
-                     WHERE medline_article_date.article_year > 2005 
-                     AND medline_article_date.article_year   < 2016
-                      "
--- src/join_med_id_to_wos_id.sql
+++
@@ -1,8 +0,0 @@
-SELECT id AS pmid,
-       article_year,
-       wosid
-FROM medline_article_date
-JOIN wosid_to_pmid
-ON
-medline_article_date.id = wosid_to_pmid.pmid::int
-;
